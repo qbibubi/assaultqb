adc04d077237c2669202ab28cd6981b11ca964bc
qbibubi <jakub.sobieraj2137@gmail.com>
2023-11-08 09:10:30 +0100

    Add SDL

--- include/gui.h
+++ include/gui.h
@@ -2,4 +2,14 @@
 
 #include "imgui.h"
 #include "imgui_impl_sdl2.h"
-#include "imgui_impl_opengl3.h"
+#include "imgui_impl_opengl3.h"	
+#include <SDL.h>
+
+namespace gui 
+{	
+	constexpr int GUI_WIDTH = 600;
+	constexpr int GUI_HEIGHT = 400;
+
+
+	void init() noexcept;
+}
--- src/gui.cpp
+++ src/gui.cpp
@@ -1,2 +1,8 @@
 #include "../include/gui.h"
 
+void gui::init() noexcept 
+{
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+
+}
